27d18686547648bfca53bad67beb67959d9ef3b1
LinRenJie <xoxosos666@gmail.com>
2022-07-19 17:32:11 +0800

    add learn vue3

--- ticket-font/vue-project/src/views/LoginView.vue
+++ ticket-font/vue-project/src/views/LoginView.vue
@@ -1,21 +1,32 @@
 <template>
-    <div>
-      登录页面
+    <div  style="background-color: blue;" >
+
+    <button @click="funclog">{{name[0]}}</button>
+    <button @click="func">{{name[1]}}</button>
+    
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent , ref } from 'vue'
 
 export default defineComponent({
     setup () {
-        
-
-        return {}
+        const name = ref(['注册','登录'])
+        const func =function func() {
+            alert('Hello vue3!')
+        }
+        const funclog  = ()=> {
+            console.log("我是注册");
+            
+        }
+        return {name,func,funclog}
     }
 })
 </script>
 
 <style scoped>
-
+ .button{
+       size:10px 
+ }
 </style>
